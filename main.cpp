7b0bfed31cd919f0b77ef1add333795d52d0a447
Matthew Endsley <mendsley@gmail.com>
2023-03-25 20:22:56 -0700

    Build an array of available characters at startup

--- main.cpp
+++ main.cpp
@@ -63,6 +63,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <algorithm>
+#include <vector>
+
 int main(int argc, char* argv[]) {
 
 	uint32_t password_len = 32;
@@ -115,23 +118,59 @@ int main(int argc, char* argv[]) {
 	uint32_t random_buffer_index = sizeof(random_buffer);
 
 	// generate character
-	constexpr char CHARACTERS[] = {
+	constexpr char ALNUM[] = {
 		  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 		, 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 		, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 		, 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 		, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-		, '!', '@', '#', '$', '%', '^', '*', '(', ')', '-', '_', '+'
+	};
+
+	constexpr char SPECIAL[] = {
+		'!', '@', '#', '$', '%', '^', '*', '(', ')', '-', '_', '+'
 		, '~', '`', '<', '>', '"', '[', ']', '{', '}', ';', ':', '\''
 	};
+
+	// build available character set
+	std::vector<char> available_characters;
+	available_characters.reserve(sizeof(ALNUM) + sizeof(SPECIAL));
+	available_characters.insert(available_characters.end(), ALNUM, ALNUM + sizeof(ALNUM));
+	if (!alnum_only) {
+		available_characters.insert(available_characters.end(), SPECIAL, SPECIAL + sizeof(SPECIAL));
+	}
+
+	if (!allow_ambiguous_chars) {
+		std::vector<char>::iterator end = std::remove_if(available_characters.begin(), available_characters.end(), [](const char c) {
+			switch (c) {
+			case 'l':
+			case '1':
+			case 'I':
+			case '|':
+			case 'O':
+			case '0':
+				return true;
+
+			default:
+				return false;
+			}
+		});
+		available_characters.erase(end, available_characters.end());
+	}
+
+	if (characters_to_remove) {
+		std::vector<char>::iterator end = std::remove_if(available_characters.begin(), available_characters.end(), [characters_to_remove](const char c) {
+			return nullptr != strchr(characters_to_remove, c);
+		});
+		available_characters.erase(end, available_characters.end());
+	}
+
+	if (available_characters.empty()) {
+		fputs("No available character set. Aborting.\n", stderr);
+		return -1;
+	}
+
 	char* alpha = new char[password_len+1u];
 	for (uint32_t ii = 0; ii != password_len; ++ii) {
-
-		const bool needs_alnum = false
-			|| (ii == 0 )
-			|| (ii == password_len-1)
-			|| alnum_only
-			;
 
 		uint32_t char_index = 0;
 		for (;;) {
@@ -151,7 +190,7 @@ int main(int argc, char* argv[]) {
 				random_buffer_index = 0;
 			}
 
-			constexpr uint8_t RANGE = 0xFF - sizeof(CHARACTERS) % 0xFF;
+			const uint8_t RANGE = 0xFF - static_cast<int>(available_characters.size()) % 0xFF;
 
 			uint8_t value = random_buffer[random_buffer_index];
 			++random_buffer_index;
@@ -159,38 +198,11 @@ int main(int argc, char* argv[]) {
 				continue;
 			}
 
-			char_index = value % sizeof(CHARACTERS);
-
-			if (needs_alnum && !isalnum(CHARACTERS[char_index])) {
-				continue;
-			} else if (nullptr != characters_to_remove) {
-				if (nullptr != strchr(characters_to_remove, CHARACTERS[char_index])) {
-					continue;
-				}
-			}
-
-			if (!allow_ambiguous_chars) {
-				bool is_ambiguous = false;
-				switch (CHARACTERS[char_index]) {
-					case 'l':
-					case '1':
-					case 'I':
-					case '|':
-					case 'O':
-					case '0':
-						is_ambiguous = true;
-						break;
-				}
-
-				if (is_ambiguous) {
-					continue;
-				}
-			}
-
+			char_index = value % static_cast<int>(available_characters.size());
 			break;
 		}
 
-		alpha[ii] = CHARACTERS[char_index];
+		alpha[ii] = available_characters[char_index];
 	}
 	alpha[password_len] = '\0';
 
